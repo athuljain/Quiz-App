d95845393e30f2eaeab59a76dd069fa707870683
athuljain <athuljainkj2@gmail.com>
2023-08-25 10:46:07 +0530

    home page commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import './App.css';
+import Main from './component/Main';
+import Quiz from "./component/Quiz";
+import Result from './component/Result';
 
 function App() {
   return (
     <div className="App">
-     <h1>React app</h1>
+     <Router>
+      <Routes>
+        <Route path='/' element={ <Main />} />
+        <Route path='/quiz' element={ <Quiz />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+     </Router>
     </div>
   );
 }
