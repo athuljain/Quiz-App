0525719039c656f88b74e2cf0d146cba8fac5384
athuljain <athuljainkj2@gmail.com>
2023-09-07 15:13:37 +0530

    result page commit

---
+++ src/component/ResultTable.jsx
@@ -0,0 +1,25 @@
+export default function ResultTable(){
+    return(
+        <div>
+            <table>
+                <thead className="table-header">
+                    <tr className="table-row">
+                        <td>name</td>
+                        <td>attemps</td>
+                        <td>earn points</td>
+                        <td>result</td>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <tr className="table-body">
+                        <td>athul</td>
+                        <td>03</td>
+                        <td>20</td>
+                        <td>passed</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
