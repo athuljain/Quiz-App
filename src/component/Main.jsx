78e0d1390b906fe2e1a571d86e1a3de454b69425
athuljain <athuljainkj2@gmail.com>
2023-10-14 13:13:45 +0530

    main style commit

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { setUserId } from "../Redux/ResultReducer";
+import "../styles/Main.css"
 
 export default function Main() {
   const inputRef = useRef(null);
@@ -20,31 +21,27 @@ export default function Main() {
       }
   }
 
-
   return (
     <div className="container">
       <h1 className="title text-light">Quiz Application</h1>
-      <ol>
-        <li>You will be asked 10 questions one after another</li>
-        <li>10 points is awarded for the correct answer</li>
-        <li>Each question has three options. You can choose only one option</li>
-        <li>You can review and chnage answers before the quiz finish</li>
-        <li>The result will be declared at the end of the quiz</li>
+      <ol className="orderd-list-main">
+        <li className="main-li">You will be asked 10 questions one after another</li>
+        <li className="main-li">10 points are awarded for the correct answer</li>
+        <li className="main-li">Each question has three options. You can choose only one option</li>
+        <li className="main-li">You can review and change answers before the quiz finishes</li>
+        <li className="main-li">The result will be declared at the end of the quiz</li>
       </ol>
       <form id="form">
         <input
           ref={inputRef}
           className="userid"
           type="text"
-          placeholder=" userName* "
-          //value={userName} // Use the input value from state
-          //onChange={handleInputChange} // Handle input changes
+          placeholder="User Name*"
         />
       </form>
       <div className="start">
-        <Link className="btn" onClick={startQuiz} to={"/quiz"}>
-          {" "}
-          Start Quiz{" "}
+        <Link className="btn" onClick={startQuiz} to="/quiz">
+          Start Quiz
         </Link>
       </div>
     </div>
